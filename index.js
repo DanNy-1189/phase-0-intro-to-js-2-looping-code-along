787478836022ccaf3cdeaa6b4b674f9985b6b304
Nancy D. Arena <ndarena11@gmail.com>
2023-05-24 20:18:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ['Charlie', 'Samip', 'Ali'];
+function writeCards(names){
+    const newCards = [];
+    for(let i = 0 ; i < names.length; i++){
+    newCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newCards;
+}
+
+function countDown(number){
+    let count = 10;
+    while (count >= 0){
+    console.log(count--);
+    }
+}
